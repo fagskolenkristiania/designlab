9ea01950f01acc2bcb162a1d820559ea8e876b56
Hans Kristian Smedsrød <eplehans@gmail.com>
2018-06-05 12:22:32 +0200

    added booking strip animnation

--- assets/scripts/bookingStrip.js
+++ assets/scripts/bookingStrip.js
@@ -1,16 +1,42 @@
 function bookingStrip(className){
 
     let bookingStrips = document.getElementsByClassName(className);
-    let bookingStrip, bookingStripText;
+
+    const itemAmount = 50;
+
+    let bookingStrip, 
+        textEl, 
+        linkEl, 
+        repeaterEl;
 
     for(let i = 0; i < bookingStrips.length; i++){
         
         bookingStrip = bookingStrips[i];
-        bookingStripText = bookingStrip.getElementsByClassName('booking-strip__text')[0].textContent;
+        bookingStrip.classList.add(`${className}--initialised`);
 
-        for(let t = 0; t < 50; t++){
-            bookingStrip.getElementsByClassName('booking-strip__repeater')[0].innerHTML += ` ${bookingStripText} •`;
+        textEl = bookingStrip.getElementsByClassName('booking-strip__text')[0];
+        linkEl = bookingStrip.getElementsByClassName('booking-strip__link')[0];
+        repeaterEl = bookingStrip.getElementsByClassName('booking-strip__repeater')[0];
+
+        const text = textEl.textContent;
+        const textElWidth = textEl.offsetWidth;
+
+        
+
+        console.log(textElWidth);
+
+        for(let t = 0; t < itemAmount; t++){
+            if(t === 0){
+                repeaterEl.innerHTML += `<span aria-hidden="false">${text}</span>`;
+            }else{
+                repeaterEl.innerHTML += `<span aria-hidden="true">${text}</span>`;
+            }
+            
+
+            linkEl.setAttribute('title', text);
+            linkEl.style.width = textElWidth*itemAmount+'px';
         }
         
     }
 }
+
---
+++ utstyr.md
@@ -0,0 +1,11 @@
+---
+layout: page
+title: Utstyr og booking
+permalink: /utstyr/
+---
+
+Dette er utstyrs-siden. 
+
+Her skal hvert element/gear presenteres, og det som trengs å bookes skal få en booking-knapp. 
+
+Idé: Lag en custom post type for utstyr og lag en post pr. enhet. 
